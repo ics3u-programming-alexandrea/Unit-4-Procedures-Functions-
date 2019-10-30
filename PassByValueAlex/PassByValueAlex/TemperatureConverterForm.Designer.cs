69391f2f10dad3df141429dfcdeb5547b721435a
s465640 <54953677+s465640@users.noreply.github.com>
2019-10-30 12:30:38 -0400

    Initial commit

---
+++ PassByValueAlex/PassByValueAlex/TemperatureConverterForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace PassByValueAlex
+{
+    partial class frmTemperatureconverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEnter = new System.Windows.Forms.Label();
+            this.txtTemperature = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblEnter
+            // 
+            this.lblEnter.AutoSize = true;
+            this.lblEnter.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnter.Location = new System.Drawing.Point(12, 34);
+            this.lblEnter.Name = "lblEnter";
+            this.lblEnter.Size = new System.Drawing.Size(446, 25);
+            this.lblEnter.TabIndex = 0;
+            this.lblEnter.Text = "Enter the temperature in degree Celcius: ";
+            // 
+            // txtTemperature
+            // 
+            this.txtTemperature.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTemperature.Location = new System.Drawing.Point(483, 34);
+            this.txtTemperature.Name = "txtTemperature";
+            this.txtTemperature.Size = new System.Drawing.Size(100, 26);
+            this.txtTemperature.TabIndex = 1;
+            
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.Location = new System.Drawing.Point(213, 100);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(118, 58);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "Convert to Farenheit";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.BtnConvert_Click);
+            // 
+            // frmTemperatureconverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 277);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtTemperature);
+            this.Controls.Add(this.lblEnter);
+            this.Name = "frmTemperatureconverter";
+            this.Text = "Temprature Converter by Alex";
+           
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEnter;
+        private System.Windows.Forms.TextBox txtTemperature;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ PassByValueAlex/PassByValueAlex/TemperatureConverterForm.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Created by: Alex Udechukwu
+ * Created on: 30-October-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #30 - Area Procedure
+ * This program converts temperature from degree celcius to degree farenheit and displays the answer in a message box
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PassByValueAlex
+{
+    public partial class frmTemperatureconverter : Form
+    {
+        public frmTemperatureconverter()
+        {
+            InitializeComponent();
+        }
+        //Funtion: convert to farenheit
+        //input: Temperature in celcius
+        //Output: void
+        //Description:This function takes the temperature in celcius and displays it in  Farenheit
+        public void ConvertToFarenheit(Double celcuis)
+        {
+            //variables
+          Double farenheit;
+
+            //write the formular for the conversion
+            farenheit = celcuis * ((double)(9) / (double)(5)) + 32;
+
+            //display the converted number in a message box plus the unit
+            MessageBox.Show(celcuis + " degree Celcius =" + farenheit + (" degree Farenheit"), "Celcius to Farenheit Converter");
+        }
+        
+        private void BtnConvert_Click(object sender, EventArgs e)
+        {
+            //declare local variabes
+            Double userCelcius;
+
+            //convert from a string to an int and assign them to their various variable
+            userCelcius = Convert.ToDouble(this.txtTemperature.Text);
+
+            //call the function to calculate the degrees in farenheit
+            this.ConvertToFarenheit(userCelcius);
+        }
+    }
+}
