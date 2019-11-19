b00302884d5fd32e5765d2232ad07b76861dd8d5
Alexandrea <54953677+s465640@users.noreply.github.com>
2019-11-18 19:37:47 -0500

    Added new walking man

--- NewWalkingManAlex/NewWalkingManAlex/NewWalkingManForm.cs
+++ NewWalkingManAlex/NewWalkingManAlex/NewWalkingManForm.cs
@@ -44,10 +44,88 @@ namespace NewWalkingManAlex
             Point pntPic = new System.Drawing.Point(x, y);
 
             //set thr location of the picture box
-            tmpPicMan.Location = pntPic; 
+            tmpPicMan.Location = pntPic;
 
             //assign the image to a picturebox 
+            tmpPicMan.Image = Properties.Resources.walk1;
+
+            //stretch the image to the size of the picture box
+            tmpPicMan.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            //make the picture box width and height to be the same as the image 
+            tmpPicMan.ClientSize = new Size(Properties.Resources.walk1.Width, Properties.Resources.walk1.Height);
+
+            //add an event listener 
+            tmpPicMan.Click += new System.EventHandler(PictureBox_Click);
+
+            //add the picture box to form 
+            this.Controls.Add(tmpPicMan);
 
         }
+        private void PictureBox_Click(object sender, EventArgs e)
+        {
+            //cast the sender object into a picture box 
+            PictureBox tmpPicMan = (PictureBox)sender;
+
+            //writea message to the console to check which picture box was clicked
+            Console.WriteLine("PictureBox(" + tmpPicMan.Location.X + ", " + tmpPicMan.Location.Y + ") was clicked.");
+
+            //declare local variable and constants 
+            const byte MAX_FRAMES = 10;
+            int pictureFrameCounter = 1;
+
+            //continue this loop while frame counter has not reached the max number of frames we have 
+            while(pictureFrameCounter< MAX_FRAMES + 1 )
+            {
+                if(pictureFrameCounter==1)
+                {
+                    tmpPicMan = Properties.Resources.walk1;
+                }
+                else if (pictureFrameCounter == 2)
+                {
+                    tmpPicMan = Properties.Resources.walk2;
+
+                }
+                else if (pictureFrameCounter == 3)
+                {
+                    tmpPicMan = Properties.Resources.walk3;
+                }
+                else if (pictureFrameCounter == 4)
+                {
+                    tmpPicMan = Properties.Resources.walk4;
+                }
+                else if (pictureFrameCounter == 5)
+                {
+                    tmpPicMan = Properties.Resources.walk5;
+                }
+                else if (pictureFrameCounter == 6)
+                {
+                    tmpPicMan = Properties.Resources.walk6;
+                }
+                else if (pictureFrameCounter == 7)
+                {
+                    tmpPicMan = Properties.Resources.walk7;
+                }
+                else if (pictureFrameCounter == 8)
+                {
+                    tmpPicMan = Properties.Resources.walk8;
+                }
+                else if (pictureFrameCounter == 9)
+                {
+                    tmpPicMan = Properties.Resources.walk9;
+                }
+                else if (pictureFrameCounter == 10)
+                {
+                    tmpPicMan = Properties.Resources.walk10;
+                }
+                //refresh the form
+                this.Refresh();
+
+                //pause the loop for 100 milleseconds
+                Thread.Sleep(100);
+            }
+
+        }
+    }
     }
 }
